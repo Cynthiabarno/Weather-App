254d55ad547640b02077aea67994f14791121067
Cynthiabarno <cynthiabarno516@gmail.com>
2023-08-24 07:51:56 +0300

    Added HTML, CSS and JS files to the app

---
+++ index.js
@@ -0,0 +1,70 @@
+let currentTime = new Date();
+
+let time = document.querySelector("#time");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentTime.getDay()];
+
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+time.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function showTemperature(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#temper").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#temperature-description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function enterCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#enter-city");
+  let city = searchInput.value;
+  searchCity(city);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("click", enterCity);
+
+function currentPosition(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric";
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function currentCity(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+
+let button = document.querySelector("#location");
+button.addEventListener("click", currentCity);
