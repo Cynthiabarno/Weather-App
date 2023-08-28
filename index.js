200293bcbcb8d77c6871c17367c1ed91a897087f
Cynthiabarno <cynthiabarno516@gmail.com>
2023-08-28 07:09:46 +0300

    Changed the weather icons and styled them

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ function showTemperature(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
